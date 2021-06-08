084d414955ba3e001e3ed04d7079c6dd725ef1b4
Bjørn Harbo Dahle <bdahle@hey.com>
2021-06-08 12:43:13 +0200

    add game board

--- app.js
+++ app.js
@@ -8,7 +8,34 @@ const gameBoardModule = (() => {
 })();
 
 // displayController
-const displayControllerModule = (() => {})();
+const displayControllerModule = (() => {
+  const gridDimensions = 500;
+  makeGrid();
+
+  function makeGrid(gridSize = 3) {
+    const tileSize = gridDimensions / gridSize;
+    container.style.gridTemplateColumns = `repeat(${gridSize}, auto)`;
+    addTiles(tileSize, gridSize);
+  }
+
+  function addTiles(tileSize, gridSize) {
+    for (i = 0; i < gridSize * gridSize; i++) {
+      const tile = makeTile(tileSize, i);
+      container.appendChild(tile);
+    }
+  }
+
+  function makeTile(tileSize, i) {
+    const tile = document.createElement("div");
+    tile.classList.add("tile");
+    tile.setAttribute("data-index", i);
+    tile.innerText = "X";
+    tile.style.backgroundColor = "cyan";
+    tile.style.width = `${tileSize}px`;
+    tile.style.height = `${tileSize}px`;
+    return tile;
+  }
+})();
 
 // multiple: factory
 // player
